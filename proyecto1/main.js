0c387535cbdc5fb12a2c9b03fc4cd7e44f018242
Maulak1588 <maurosanzberro@gmail.com>
2024-01-17 19:52:16 -0300

    test4

---
+++ proyecto1/main.js
@@ -0,0 +1,15 @@
+let url = 'https://jsonplaceholder.typicode.com/users'
+const datos = fetch(url)
+    .then(res => res.json())
+    .then(info => mostrarData(info))
+    .catch(err =>console.log(err))
+
+function mostrarData(info) {
+    console.log(info)
+    document.getElementById('name').innerHTML = info[0].name
+    document.getElementById('mail').innerHTML = info[0].email
+    document.getElementById('phone').innerHTML = info[0].phone
+    document.getElementById('firstName').innerHTML = info[0].name.split(' ')[0]
+}
+
+function a(){}
